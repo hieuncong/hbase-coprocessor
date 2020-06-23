d5765e618d294bf911f4b36c9207c82e1c16e470
hieunc <431-hieunc@lab.admicro.vn>
2020-06-23 16:14:06 +0700

    FEAT: omit data from scan operator

--- src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
+++ src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
@@ -4,13 +4,16 @@ import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellBuilderFactory;
 import org.apache.hadoop.hbase.CellBuilderType;
 import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessor;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.coprocessor.RegionObserver;
+import org.apache.hadoop.hbase.regionserver.InternalScanner;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,5 +45,20 @@ public class RegionObserverExample implements RegionCoprocessor, RegionObserver 
             results.add(c);
             e.bypass();
         }
+    }
+
+    @Override
+    public boolean postScannerNext(final ObserverContext<RegionCoprocessorEnvironment> e, final InternalScanner s,
+                                   final List<Result> results, final int limit, final boolean hasMore) throws IOException {
+        Result result = null;
+        Iterator<Result> iterator = results.iterator();
+        while (iterator.hasNext()) {
+            result = iterator.next();
+            if (Bytes.equals(result.getRow(), ROW)) {
+                iterator.remove();
+                break;
+            }
+        }
+        return hasMore;
     }
 }

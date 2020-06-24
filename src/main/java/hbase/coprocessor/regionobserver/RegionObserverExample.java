642da28575191f4db1cca15942fe0853b22d130a
hieunc <431-hieunc@lab.admicro.vn>
2020-06-24 09:38:58 +0700

    REFACTOR: update variables' values

--- src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
+++ src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
@@ -25,9 +25,9 @@ import java.util.Optional;
 public class RegionObserverExample implements RegionCoprocessor, RegionObserver {
     private static final byte[] ADMIN = Bytes.toBytes("admin");
     private static final byte[] EMP = Bytes.toBytes("emp");
-    private static final byte[] COLUMN_FAMILY = Bytes.toBytes("details");
-    private static final byte[] COLUMN = Bytes.toBytes("Admin_det");
-    private static final byte[] VALUE = Bytes.toBytes("You can't see Admin details");
+    private static final byte[] COLUMN_FAMILY = Bytes.toBytes("family");
+    private static final byte[] COLUMN = Bytes.toBytes("qualifier");
+    private static final byte[] VALUE = Bytes.toBytes("You can't see this row");
 
     @Override
     public Optional<RegionObserver> getRegionObserver() {

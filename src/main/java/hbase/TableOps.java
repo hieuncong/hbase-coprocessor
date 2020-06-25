8dc8974066785d33ce0c81bf27b868c1e5bdf7b5
hieunc <431-hieunc@lab.admicro.vn>
2020-06-25 16:57:34 +0700

    FEAT: sum endpoint example

--- src/main/java/hbase/TableOps.java
+++ src/main/java/hbase/TableOps.java
@@ -1,5 +1,7 @@
 package hbase;
 
+import com.google.protobuf.ServiceException;
+import hbase.coprocessor.endpoint.Sum;
 import hbase.coprocessor.regionobserver.RegionObserverExample;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -7,10 +9,12 @@ import org.apache.hadoop.hbase.Coprocessor;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.io.compress.Compression;
+import org.apache.hadoop.hbase.client.coprocessor.Batch;
+import org.apache.hadoop.hbase.ipc.CoprocessorRpcUtils;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class TableOps {
     private static Configuration config;
@@ -32,7 +36,39 @@ public class TableOps {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(table.getDescriptor());
+        sumEndpointExample();
+    }
+
+    public static void sumEndpointExample() {
+        final Sum.SumRequest request = Sum.SumRequest.newBuilder().setFamily("f1").setColumn("a").build();
+        try {
+            Map<byte[], Long> results = table.coprocessorService(
+                    Sum.SumService.class,
+                    null,  /* start key */
+                    null,  /* end   key */
+                    new Batch.Call<Sum.SumService, Long>() {
+                        @Override
+                        public Long call(Sum.SumService aggregate) throws IOException {
+                            CoprocessorRpcUtils.BlockingRpcCallback<Sum.SumResponse> rpcCallback = new CoprocessorRpcUtils.BlockingRpcCallback<>();
+                            aggregate.getSum(null, request, rpcCallback);
+                            Sum.SumResponse response = rpcCallback.get();
+
+                            return response.hasSum() ? response.getSum() : 0L;
+                        }
+                    }
+            );
+
+            for (Long sum : results.values()) {
+                System.out.println("Sum = " + sum);
+            }
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateTableAtt() throws IOException {
         String path = "hdfs://master:9000/user/hadoop/hbase-coprocessor-1.0.jar";
         TableDescriptor tableDescriptor = TableDescriptorBuilder
                 .newBuilder(tableName)

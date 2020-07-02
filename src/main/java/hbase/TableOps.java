4927c5444d4ee3ec5ab84c4eceb8a0dec64b9167
hieunc <431-hieunc@lab.admicro.vn>
2020-07-02 15:22:28 +0700

    FEAT: check resource file

--- src/main/java/hbase/TableOps.java
+++ src/main/java/hbase/TableOps.java
@@ -20,15 +20,18 @@ public class TableOps {
     private static Configuration config;
     private static Connection connection;
     private static Admin admin;
-    private static TableName tableName = TableName.valueOf("test");
-    private static Table table;
+    private static TableName tableName1 = TableName.valueOf("test");
+    private static Table table1;
+    private static TableName tableName2 = TableName.valueOf("test2");
+    private static Table table2;
 
     static {
         try {
             config = HBaseConfiguration.create();
             config.addResource(new Path("src/main/resources/hbase-site-vm.xml"));
             connection = ConnectionFactory.createConnection(config);
-            table = connection.getTable(tableName);
+            table1 = connection.getTable(tableName1);
+            table2 = connection.getTable(tableName2);
             admin = connection.getAdmin();
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,14 +39,20 @@ public class TableOps {
     }
 
     public static void main(String[] args) throws IOException {
-        sumEndpointExample();
+        System.out.println(config);
+//        updateTableAtt();
+//        Put p = new Put(Bytes.toBytes("r1"));
+//        p.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("a"), Bytes.toBytes("v1"));
+//        p.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("b"), Bytes.toBytes("v2"));
+//
+//        table1.put(p);
     }
 
     public static void sumEndpointExample() throws IOException {
-        System.out.println(table.getDescriptor());
+        System.out.println(table1.getDescriptor());
         final Sum.SumRequest request = Sum.SumRequest.newBuilder().setFamily("f1").setColumn("a").build();
         try {
-            Map<byte[], Long> results = table.coprocessorService(
+            Map<byte[], Long> results = table1.coprocessorService(
                     Sum.SumService.class,
                     null,  /* start key */
                     null,  /* end   key */
@@ -72,7 +81,7 @@ public class TableOps {
     public static void updateTableAtt() throws IOException {
         String regionObserver = "hdfs://master:9000/user/hadoop/hbase-coprocessor-regionobserver-1.0.jar";
         TableDescriptor tableDescriptor = TableDescriptorBuilder
-                .newBuilder(tableName)
+                .newBuilder(tableName2)
                 .setValue("COPROCESSOR$1", regionObserver + "|"
                         + RegionObserverExample.class.getCanonicalName() + "|"
                         + Coprocessor.PRIORITY_USER)

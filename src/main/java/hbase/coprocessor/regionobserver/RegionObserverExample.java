ed9f5521c185742777780c4148725423cd5629ea
hieunc <431-hieunc@lab.admicro.vn>
2020-07-02 14:57:54 +0700

    FEAT: pre put example

--- src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
+++ src/main/java/hbase/coprocessor/regionobserver/RegionObserverExample.java
@@ -22,9 +22,37 @@ public class RegionObserverExample implements RegionCoprocessor, RegionObserver 
     private static final byte[] COLUMN = Bytes.toBytes("qualifier");
     private static final byte[] VALUE = Bytes.toBytes("You can't see this row");
 
+    private Connection connection;
+
     @Override
     public Optional<RegionObserver> getRegionObserver() {
         return Optional.of(this);
+    }
+
+    @Override
+    public void start(CoprocessorEnvironment env) throws IOException {
+        Configuration config = env.getConfiguration();
+        connection = ConnectionFactory.createConnection(config);
+    }
+
+    @Override
+    public void stop(CoprocessorEnvironment env) throws IOException {
+        connection.close();
+    }
+
+    @Override
+    public void prePut(ObserverContext<RegionCoprocessorEnvironment> c, Put put, WALEdit edit, Durability durability) throws IOException {
+        byte[] row1 = put.getRow();
+        byte[] value1 = CellUtil.cloneValue(put.get(Bytes.toBytes("f1"), Bytes.toBytes("a")).get(0));
+        byte[] value2 = CellUtil.cloneValue(put.get(Bytes.toBytes("f1"), Bytes.toBytes("b")).get(1));
+
+        Put put2 = new Put(value1);
+        put2.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("row"), row1);
+        put2.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("b"), value2);
+
+        TableName tableName2 = TableName.valueOf("test2");
+        Table table2 = connection.getTable(tableName2);
+        table2.put(put2);
     }
 
     @Override
